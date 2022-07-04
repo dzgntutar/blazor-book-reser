76fa8ffad960e452c507e6cb8b9bb2ed56026296
dzgntutar <duzguntutar@gmail.com>
2022-07-04 19:18:24 +0300

    Automapper

---
+++ BookReservation/Server/Services/Extensions/ConfigureMappingExtension.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BookReservation.Data.Entities;
+using BookReservation.Shared.Dtos;
+
+namespace BookReservation.Server.Services.Extensions
+{
+    public static class ConfigureMappingExtension
+    {
+        public static IServiceCollection ConfigureMapping(this IServiceCollection service)
+        {
+            var mappingConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingProfile()); });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+
+            service.AddSingleton(mapper);
+
+            return service;
+        }
+
+        public class MappingProfile : Profile
+        {
+            public MappingProfile()
+            {
+                AllowNullDestinationValues = true;
+                AllowNullCollections = true;
+
+                CreateMap<Book, BookGetAllDto>();
+                CreateMap<Book, BookGetByIdDto>();
+            }
+        }
+    }
+}
