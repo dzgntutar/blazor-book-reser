f27948c2d240a9a6a6fe6ecb2f54aa54e53a8ab2
dzgntutar <duzguntutar@gmail.com>
2022-07-25 23:06:47 +0300

    Todo example page

---
+++ BookReservation/Server/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookReservation.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        
+    }
+}
