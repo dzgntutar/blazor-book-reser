f3a6360972181e204ac2cb561a16dcd63d21ae83
dzgntutar <duzguntutar@gmail.com>
2022-08-22 18:08:18 +0300

    User add or update form

--- BookReservation/Shared/Dtos/User/UserSaveDto.cs
+++ BookReservation/Shared/Dtos/User/UserSaveDto.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookReservation.Shared.Dtos;
 
 namespace BookReservation.Shared.Dtos.User
 {
-    public class UserGetByIdDto : BaseDto
+    public class UserSaveDto : BaseDto
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
